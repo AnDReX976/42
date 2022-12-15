e4d8e882794e54352069676dea4fb4eda130b89c
AnDReX976 <120676267+AnDReX976@users.noreply.github.com>
2022-12-15 22:17:00 +0100

    Aggiorna pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 basic.showString("Hello")
-basic.showString("Quanto fa 4+2?")
-basic.showString("" + (4 + 2))
-music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+basic.showString("2>4?")
+if ((2 as any) > (4 as any)) {
+    basic.showIcon(IconNames.Yes)
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+} else {
+    basic.showIcon(IconNames.No)
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+    music.setVolume(255)
+}
